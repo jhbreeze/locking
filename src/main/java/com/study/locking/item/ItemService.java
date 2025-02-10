c204b61ee304b7f202f834cbc11ed0d17e51dde4
jaehee-An <jhh9615@gmail.com>
2025-02-10 18:29:22 +0900

    dead lock

--- src/main/java/com/study/locking/item/ItemService.java
+++ src/main/java/com/study/locking/item/ItemService.java
@@ -1,6 +1,7 @@
 package com.study.locking.item;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
@@ -22,5 +23,22 @@ public class ItemService {
 	public Item findItemById(Long itemId) {
 		// 비관적 락 없이 데이터를 조회합니다.
 		return itemRepository.findById(itemId).orElse(null);
+	}
+
+	@Transactional(timeout = 1, isolation = Isolation.SERIALIZABLE)
+	public void updateItemsQuantity(Long itemId1, Long itemId2) {
+		Item item1 = itemRepository.findByIdWithLock(itemId1);
+		item1.setQuantity(item1.getQuantity() + 10);
+		itemRepository.save(item1);
+
+		try {
+			Thread.sleep(4);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		Item item2 = itemRepository.findByIdWithLock(itemId2);
+		item2.setQuantity(item2.getQuantity() + 20);
+		itemRepository.save(item2);
 	}
 }
